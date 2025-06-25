82c97e7b05847310839a28ab659ff5a135fcd855
Danny Titkov <groverdk@gmail.com>
2025-06-25 17:29:10 +1000

    add misplaced letters displaying

--- game.py
+++ game.py
@@ -16,13 +16,28 @@ def check_guess(player_word, secret_word) -> int:
     secret_letters = [letter.lower() for letter in secret_word]
     letters_on_screen = []
     guessed_idx = []
+    unguessed_letters = []
 
+    # Проверяем буквы на точное совпадение по их месту в слове.
     for idx, letter in enumerate(secret_letters):
         if secret_letters[idx] == player_letters[idx]:
             letters_on_screen.append(letter.upper())
             guessed_idx.append(idx)
         else:
             letters_on_screen.append('_')
+            unguessed_letters.append(letter)
+
+    # Проверяем оставшиеся буквы из догадки на их наличие в слове.
+    for idx, letter in enumerate(player_letters):
+        if idx in guessed_idx:
+            continue
+
+        # Если буква игрока есть в списке неугаданных букв, то вставляем
+        # ее в нижнем регистре в список букв, которые появятся на экране.
+        # Удаялем эту букву из списка неугаданных.
+        if letter in unguessed_letters:
+            letters_on_screen[idx] = letter
+            unguessed_letters.pop(unguessed_letters.index(letter))
 
     print(' '.join(letters_on_screen), "\n")
 
@@ -40,11 +55,16 @@ def game():
             print("You are right!")
             break
         else:
-            print("It's not the right word.\n")
             attempts += 1
+            print(
+                f"It's not the right word. Attempts left: {TRIES - attempts}."
+            )
 
         if attempts == TRIES:
-            print("Sorry, you lost this time. Please try again!")
+            print(
+                "Sorry, you lost this time. Please try again!\n"
+                f"The word was {secret_word.upper()}."
+            )
             break
 
 
